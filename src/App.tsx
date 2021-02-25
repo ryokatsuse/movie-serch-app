937d6b5130e3f5c20cf260191e2aa73415a9d4d0
Ryo Katsuse <ryokatsu0719@gmail.com>
2021-02-26 06:21:29 +0900

    fix:useReducerに置き換える

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,79 @@
 import './App.css'
-import React, { useState, useEffect } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import GlobalHeader from './views/components/GlobalHeader'
 import Movie from './views/components/Movie'
 import Search from './views/components/Search'
 
 const API = 'https://www.omdbapi.com/?s=man&apikey=2d324c44'
 
+const initialState = {
+  loding: true,
+  movies: [],
+  errorMessage: null,
+}
+
+const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case 'SEARCH_MOVIES_REQUEST':
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null,
+      }
+    case 'SEARCH_MOVIES_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload,
+      }
+    case 'SEARCH_MOVIES_FAILURE':
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error,
+      }
+    default:
+      return state
+  }
+}
+
 const App: React.FC = () => {
-  const [loading, setLoading] = useState(true)
-  const [movies, setMovies] = useState([])
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
     fetch(API)
       .then((response) => response.json())
       .then((jsonResponse) => {
-        setMovies(jsonResponse.Search)
-        setLoading(false)
+        dispatch({
+          type: 'SEARCH_MOVIES_SUCCESS',
+          payload: jsonResponse.Search,
+        })
       })
   }, [])
 
   const search = (searchValue: any) => {
-    setLoading(true)
-    setErrorMessage(null)
+    dispatch({
+      type: 'SEARCH_MOVIES_REQUEST',
+    })
 
     fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=2d324c44`)
       .then((response) => response.json())
       .then((jsonResponse) => {
         if (jsonResponse.Response === 'True') {
-          setMovies(jsonResponse.Search)
-          setLoading(false)
+          dispatch({
+            type: 'SEARCH_MOVIES_SUCCESS',
+            payload: jsonResponse.Search,
+          })
         } else {
-          setErrorMessage(jsonResponse.Error)
-          setLoading(false)
+          dispatch({
+            type: 'SEARCH_MOVIES_FAILURE',
+            payload: jsonResponse.Error,
+          })
         }
       })
   }
+
+  const { movies, errorMessage, loading } = state
 
   return (
     <div className="App">
@@ -48,7 +86,7 @@ const App: React.FC = () => {
         ) : errorMessage ? (
           <div className="errorMessage">{errorMessage}</div>
         ) : (
-          movies.map((movie, index) => (
+          movies.map((movie: any, index: any) => (
             <Movie key={`${index}-${(movie as any).title}`} movie={movie} />
           ))
         )}
